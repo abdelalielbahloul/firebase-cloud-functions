87a091070efdb8c632ad6f992b95c1049364ab79
Abdelali <El BAHLOUL>
2020-01-07 21:47:20 +0100

    renaming importing file using @google-cloud/storage package

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+const { gcs } = require('@google-cloud/storage');
+const os = require('os');
+const path = require('path');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,7 +10,31 @@ const functions = require('firebase-functions');
 //  response.send("Hello from Firebase!");
 // });
 exports.onFileImport = functions.storage.object().onFinalize(event => {
- console.log(event);
- return;
+    // we want to rename file imported
+    const object = event.data;
+    const bucket = object.bucket;
+    const contentType = object.contentType;
+    const filePath = object.name;
+    console.log("Funcion change detected");
+
+    //check if we have already triggered this function 
+    if (path.basename(filePath).startsWith('renamedTo-')) {
+        console.log("You have already renamed that file");
+        
+        return;
+    }
+    const destBucket = gcs.bucket(bucket);
+    const tempFilePath = path.join(os.tmpdir(), path.basename(filePath));
+    const metaData = { contentType: contentType };
+    
+    return destBucket.file(filePath).download({
+        destination: tempFilePath
+    }).then(() => {
+        return destBucket.upload(tempFilePath, {
+            destination: `renamedTo- ${path.basename(filePath)}`,
+            metaData: metaData
+        })
+    })
+    
  
 });
